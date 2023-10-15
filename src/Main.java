d623bf43289bdc1a4472abe3efdc1d8f184bab01
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 18:43:07 -0300

    chore: Change sout messages

--- src/Main.java
+++ src/Main.java
@@ -5,75 +5,80 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        // utilizada para obter entradas de dados do usuário, como teclado/arquivo
-        // System.in - input stream, rastreia entradas
-        Scanner scanner = new Scanner(System.in);
-        String welcomeMessage = """
-                  Hello world!
-                  This is a simple calculator project.""";
-        System.out.println(welcomeMessage);
+    public static void main(String[] args) throws InterruptedException {
+        try {
+            // utilizada para obter entradas de dados do usuário, como teclado/arquivo
+            // System.in - input stream, rastreia entradas
+            Scanner scanner = new Scanner(System.in);
+            String welcomeMessage = """
+                    ___________________________________________________
+                    Hello world!
+                    This is a simple calculator project.""";
+            System.out.println(welcomeMessage);
+            Thread.sleep(500);
 
-        String operationMessage = """
-                First, let's choose an operation.
-                1. Addition
-                2. Subtraction
-                3. Multiplication
-                4. Division
-                5. Average (of values in an array)
-                0. Exit
-                """;
+            String operationMessage = """
+                    ____________________________________________________
+                    First, let's choose an operation.
+                    1. Addition
+                    2. Subtraction
+                    3. Multiplication
+                    4. Division
+                    5. Average (of values in an array)
+                    0. Exit
+                    ____________________________________________________
+                    """;
 
+            System.out.println(operationMessage);
+            Thread.sleep(500);
 
-        System.out.println(operationMessage);
+            int operation = Utils.getOperation(scanner, "Operation: ");
 
-        int operation = Utils.getOperation(scanner, "Type a double value");
-
-        switch (operation) {
-            case 1:
-                System.out.println("Addition: ");
-                ArrayList<Double> arraySum = Utils.setArray(scanner, "Type a " +
-                        "double value: ");
-                Calculator.sum(arraySum);
-                double sum = Calculator.sum(arraySum);
-                System.out.printf("The sum of the numbers in the array is: %s",
-                sum);
-                break;
-            case 2:
-                System.out.println("Subtraction: ");
-                ArrayList<Double> arraySubtraction = Utils.setArray(scanner,
-                        "Type a " +
-                "double value: ");
-                double subtraction = Calculator.subtract(arraySubtraction);
-                System.out.printf("The subtraction of the numbers in the " +
-                "array is: %s", subtraction);
-                break;
-            case 3:
-                ArrayList<Double> arrayMultiplication = Utils.setArray(scanner,
-                        "Type a " +
-                                "double value: ");
-                double multiply = Calculator.multiply(arrayMultiplication);
-                System.out.printf("The multiplication of the numbers in the " +
-                "array is: %s", multiply);
-                break;
-            case 4:
-                ArrayList<Double> arrayDivision = Utils.setArray(scanner,
-                        "Type a " +
-                                "double value: ");
-                double division = Calculator.divide(arrayDivision);
-                System.out.printf("The division of the numbers in the " +
-                        "array is: %s", division);
-                break;
-            case 5:
-                ArrayList<Double> arrayAverage = Utils.setArray(scanner,
-                        "Type a " +
-                                "double value: ");
-                double average = Calculator.average(arrayAverage);
-                System.out.printf("The average of the numbers in the " +
-                        "array is: %s", average);
-                break;
-            default:
-                System.out.println("Invalid choice.");
+            switch (operation) {
+                case 1:
+                    System.out.println("Addition: ");
+                    ArrayList<Double> arraySum = Utils.setArray(scanner);
+                    Calculator.sum(arraySum);
+                    double sum = Calculator.sum(arraySum);
+                    System.out.printf("The sum of the numbers in the array is: %s",
+                            sum);
+                    break;
+                case 2:
+                    System.out.println("Subtraction: ");
+                    ArrayList<Double> arraySubtraction = Utils.setArray(scanner);
+                    double subtraction = Calculator.subtract(arraySubtraction);
+                    System.out.printf("The subtraction of the numbers in the " +
+                            "array is: %s", subtraction);
+                    break;
+                case 3:
+                    System.out.println("Multiplication: ");
+                    ArrayList<Double> arrayMultiplication = Utils.setArray(scanner);
+                    double multiply = Calculator.multiply(arrayMultiplication);
+                    System.out.printf("The multiplication of the numbers in the " +
+                            "array is: %s", multiply);
+                    break;
+                case 4:
+                    System.out.println("Division: ");
+                    ArrayList<Double> arrayDivision = Utils.setArray(scanner);
+                    double division = Calculator.divide(arrayDivision);
+                    System.out.printf("The division of the numbers in the " +
+                            "array is: %s", division);
+                    break;
+                case 5:
+                    System.out.println("Average: ");
+                    ArrayList<Double> arrayAverage = Utils.setArray(scanner);
+                    double average = Calculator.average(arrayAverage);
+                    System.out.printf("The average of the numbers in the " +
+                            "array is: %s", average);
+                    break;
+                case 0:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
+            }
+        } catch (InterruptedException error) {
+            error.printStackTrace();
         }
     }
 }
