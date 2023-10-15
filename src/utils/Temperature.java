acb8b6f37ed3ac3fc66abc26a89a8c22f7a3a662
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 13:50:06 -0300

    chore: Remove unused files

--- src/utils/Temperature.java
+++
@@ -1,9 +0,0 @@
-package utils;
-
-public class Temperature {
-    public static double celsiusToFahrenheit(double temp) {
-        double result = (temp * 1.8) + 32;
-        System.out.println(result);
-        return result;
-    }
-}
