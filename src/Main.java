acad80416a6d8c390a5cb95aa9026bd7040f053f
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 13:50:24 -0300

    chore: Remove unused lines

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
-import utils.Temperature;
+
 
 public class Main {
 
     public static void main(String[] args) {
-//      double temperature = 35.5;
-//      Temperature.celsiusToFahrenheit(temperature);
-
-//      Examples.basics();
-//      Examples.forLoop();
-//      Examples.localAndDate();
-
-//      int[] arr = { 1, 2, 3, 4, 5};
-//      Examples.arrays(arr);
-
-//      Examples.textBlock();
     }
 }
