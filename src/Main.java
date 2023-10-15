35a5a3cd5e24d489d722c45b438fc97219343579
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 19:17:55 -0300

    feat: Add guessing game implementation

--- src/Main.java
+++ src/Main.java
@@ -1,81 +1,63 @@
 import utils.Utils;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
         try {
-            // utilizada para obter entradas de dados do usuário, como teclado/arquivo
-            // System.in - input stream, rastreia entradas
+            int counter = 0;
             Scanner scanner = new Scanner(System.in);
-            String welcomeMessage = """
+            System.out.println("""
                     ___________________________________________________
                     Hello world!
-                    This is a simple calculator project.""";
-            System.out.println(welcomeMessage);
+                    This is a guessing game!""");
             Thread.sleep(500);
 
-            String operationMessage = """
+            System.out.println("""
                     ____________________________________________________
-                    First, let's choose an operation.
-                    1. Addition
-                    2. Subtraction
-                    3. Multiplication
-                    4. Division
-                    5. Average (of values in an array)
-                    0. Exit
-                    ____________________________________________________
-                    """;
-
-            System.out.println(operationMessage);
+                    The application will sort a random number from 1 to 100,
+                    and you have to guess what that number is.""");
             Thread.sleep(500);
 
-            int operation = Utils.getOperation(scanner, "Operation: ");
+            System.out.println("""
+                    ____________________________________________________
+                    You have five chances.
+                    Good luck!""");
+            Thread.sleep(500);
 
-            switch (operation) {
-                case 1:
-                    System.out.println("Addition: ");
-                    ArrayList<Double> arraySum = Utils.setArray(scanner);
-                    Calculator.sum(arraySum);
-                    double sum = Calculator.sum(arraySum);
-                    System.out.printf("The sum of the numbers in the array is: %s",
-                            sum);
-                    break;
-                case 2:
-                    System.out.println("Subtraction: ");
-                    ArrayList<Double> arraySubtraction = Utils.setArray(scanner);
-                    double subtraction = Calculator.subtract(arraySubtraction);
-                    System.out.printf("The subtraction of the numbers in the " +
-                            "array is: %s", subtraction);
-                    break;
-                case 3:
-                    System.out.println("Multiplication: ");
-                    ArrayList<Double> arrayMultiplication = Utils.setArray(scanner);
-                    double multiply = Calculator.multiply(arrayMultiplication);
-                    System.out.printf("The multiplication of the numbers in the " +
-                            "array is: %s", multiply);
-                    break;
-                case 4:
-                    System.out.println("Division: ");
-                    ArrayList<Double> arrayDivision = Utils.setArray(scanner);
-                    double division = Calculator.divide(arrayDivision);
-                    System.out.printf("The division of the numbers in the " +
-                            "array is: %s", division);
-                    break;
-                case 5:
-                    System.out.println("Average: ");
-                    ArrayList<Double> arrayAverage = Utils.setArray(scanner);
-                    double average = Calculator.average(arrayAverage);
-                    System.out.printf("The average of the numbers in the " +
-                            "array is: %s", average);
-                    break;
-                case 0:
+            int number = Utils.getRandomNumber();
+
+            while(counter <= 5) {
+                String message = String.format("""
+                        ____________________________________________________
+                        Guess #%d: """, counter);
+                int guess = Utils.getInput(scanner, message);
+                counter += 1;
+
+                if (guess > number) {
+                    Thread.sleep(250);
+                    System.out.println("""
+                            ____________________________________________________
+                            Your guess is bigger than the secret number.""");
+                } else if (guess < number) {
+                    Thread.sleep(250);
+                    System.out.println("""
+                            ____________________________________________________
+                            Your guess is smaller than the secret number.""");
+                } else {
+                    Thread.sleep(250);
+                    System.out.printf("""
+                            ____________________________________________________
+                            COUNGRATULATIONS!!!
+                            The secret number is %d!%n""", number);
+                    if (counter > 0) {
+                        System.out.printf("""
+                            You took %d chances to guess.%n""", counter - 1);
+                    }
                     System.exit(0);
                     break;
-                default:
-                    System.out.println("Invalid choice.");
+                }
             }
         } catch (InterruptedException error) {
             error.printStackTrace();
