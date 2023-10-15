c9f07a439456c64254fc93f3375603f4be99fbaf
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 17:31:43 -0300

    refactor: Refactor averageArray and sumArray methods

--- src/Calculator.java
+++ src/Calculator.java
@@ -23,10 +23,28 @@ public class Calculator {
 
     public static double sumArray(ArrayList<Double> array) {
         double result = 0;
-        for (double value : array) {
-            result += value;
+        StringBuilder response = new StringBuilder();
+        for (int i = 0; i < array.size(); i += 1) {
+            double value = array.get(i);
+            if (value != -1) {
+                result += value;
+                response.append(value).append(", ");
+            }
         }
-//        System.out.println(result);
+//        System.out.printf("The sum of %s is: %s", response.toString(), result);
         return result;
+    }
+
+    public static void averageArray(ArrayList<Double> array) {
+        double sum = sumArray(array);
+        double result = sum / array.size();
+        StringBuilder response = new StringBuilder();
+        for (int i = 0; i < array.size(); i += 1) {
+            double value = array.get(i);
+            if (value != -1) {
+                response.append(value).append(", ");
+            }
+        }
+        System.out.printf("The average of %s is: %s", response.toString(), result);
     }
 }
