415f0f4299735f930df9a8f9a9cca225c5ded762
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 15:23:44 -0300

    feat: Implement Calculator using Scanner inputs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,36 @@
-
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        // utilizada para obter entradas de dados do usuário, como teclado/arquivo
+        // System.in - input stream, rastreia entradas
+        Scanner scanner = new Scanner(System.in);
+
+        String welcomeMessage = """
+        Hello world!
+        This is a simple calculator project""";
+        System.out.println(welcomeMessage);
+
+        System.out.println("Type an integer number: ");
+        if(scanner.hasNextInt()) {
+            // lê a linha de texto da entrada do usuário e a retorna como uma
+            // string - aguarda até que o usuário pressione Enter após inserir o
+            // texto
+            int a = scanner.nextInt();
+            System.out.println("Type another integer number: ");
+            if(scanner.hasNextInt()) {
+                int b = scanner.nextInt();
+                scanner.nextLine();
+                Calculator.sum(a, b);
+                Calculator.subtract(a, b);
+                Calculator.multiply(a, b);
+                Calculator.divide(a, b);
+            } else {
+                System.out.println("Input must be an integer");
+            }
+        } else {
+            System.out.println("Input must be an integer");
+        }
     }
 }
