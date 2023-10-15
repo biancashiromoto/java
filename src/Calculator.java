5761b8128387821f255b00417b1c2972531304e1
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 18:26:52 -0300

    refactor: Methods subtract, multiply, divide, sum, average and subtraction

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,28 +1,47 @@
+import utils.Utils;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Calculator {
-    public static void sum(double a, double b) {
-        double result = a + b;
-        System.out.printf("%s + %s = %s %n", a, b, result);
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static double subtract(ArrayList<Double> array) {
+        // inicia result como o primeiro item do array
+        double result = array.get(0);
+        // inicia i em 1 porque já usamos o primeiro elemento no result
+        for (int i = 1; i < array.size(); i++) {
+            double value = array.get(i);
+            if (value != -1) {
+                result = result - value;
+            }
+        }
+        return result;
     }
 
-    public static void subtract(double a, double b) {
-        double result = a - b;
-        System.out.printf("%s - %s = %s %n", a, b, result);
+    public static double multiply(ArrayList<Double> array) {
+        double result = array.get(0);
+        for (int i = 1; i < array.size(); i++) {
+            double value = array.get(i);
+            if (value != -1) {
+                result = result * value;
+            }
+        }
+        return result;
     }
 
-    public static void multiply(double a, double b) {
-        double result = a * b;
-        System.out.printf("%s * %s = %s %n", a, b, result);
+    public static double divide(ArrayList<Double> array) {
+        double result = array.get(0);
+        for (int i = 1; i < array.size(); i++) {
+            double value = array.get(i);
+            if (value != -1) {
+                result = result / value;
+            }
+        }
+        return result;
     }
 
-    public static void divide(double a, double b) {
-        double result = a / b;
-        System.out.printf("%s / %s = %s %n", a, b, result);
-    }
-
-    public static double sumArray(ArrayList<Double> array) {
-        double result = 0;
+    public static double sum(ArrayList<Double> array) {
+        double result = array.get(0);
         StringBuilder response = new StringBuilder();
         for (int i = 0; i < array.size(); i += 1) {
             double value = array.get(i);
@@ -31,20 +50,11 @@ public class Calculator {
                 response.append(value).append(", ");
             }
         }
-//        System.out.printf("The sum of %s is: %s", response.toString(), result);
         return result;
     }
 
-    public static void averageArray(ArrayList<Double> array) {
-        double sum = sumArray(array);
-        double result = sum / array.size();
-        StringBuilder response = new StringBuilder();
-        for (int i = 0; i < array.size(); i += 1) {
-            double value = array.get(i);
-            if (value != -1) {
-                response.append(value).append(", ");
-            }
-        }
-        System.out.printf("The average of %s is: %s", response.toString(), result);
+    public static double average(ArrayList<Double> array) {
+        double sum = sum(array);
+        return sum / array.size();
     }
 }
