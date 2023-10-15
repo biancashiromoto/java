dc74c6a7603251e62922257328715af390c69726
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 11:18:21 -0300

    fix: Fix indentation

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,16 @@ import utils.Temperature;
 public class Main {
 
     public static void main(String[] args) {
-//        double temperature = 35.5;
-//        Temperature.celsiusToFahrenheit(temperature);
+//      double temperature = 35.5;
+//      Temperature.celsiusToFahrenheit(temperature);
 
-//        Examples.basics();
-//        Examples.forLoop();
-//        Examples.localAndDate();
+//      Examples.basics();
+//      Examples.forLoop();
+//      Examples.localAndDate();
 
-//        int[] arr = { 1, 2, 3, 4, 5};
-//        Examples.arrays(arr);
+//      int[] arr = { 1, 2, 3, 4, 5};
+//      Examples.arrays(arr);
 
-//        Examples.textBlock();
-
-
+//      Examples.textBlock();
     }
 }
