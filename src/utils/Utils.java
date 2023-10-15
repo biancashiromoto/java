a570756deabf57838dd4f9768e5afed1c2733876
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-15 16:04:58 -0300

    refactor: Refactor getInput method

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -3,17 +3,23 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Utils {
-    public static int getInput(Scanner scanner, String message) {
-        boolean isValidInput = false;
-        int input = -1;
+    public static double getInput(Scanner scanner, String message) {
+        // define o valor inicial de input, para poder retorná-lo depois
+        double input = -1;
 
-        while(!isValidInput) {
+        // criação de um loop infinito, que só para quando usamos break
+        while(true) {
+            // imprime a mensagem no console
             System.out.println(message);
             try {
-                input = scanner.nextInt();
-                isValidInput = true;
+                // caso o valor seja um double, atribui à input e dá um break
+                // no while loop
+                input = scanner.nextDouble();
+                break;
             } catch (InputMismatchException error) {
-                System.out.println("Input must be an integer");
+                // caso contrário, imprime a mensagem de erro
+                System.out.println("Input must be a double value");
+                // a mensagem de erro é exibida e a entrada é limpa
                 scanner.next();
             }
         }
