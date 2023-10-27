202a47a6844a40b0641c854cd0e869a4fe6a535b
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-27 19:19:31 -0300

    feat: Implement data fetch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,10 @@
+import utils.Pokemon;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Pokemon pokemon = new Pokemon();
+        String response = pokemon.fetch();
+        System.out.println(response);
     }
 }
