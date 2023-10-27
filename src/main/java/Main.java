c9a3300c3113610547e43fb53b22146025091b73
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-27 20:47:04 -0300

    feat: Add welcomeMessage and sleep between messages

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,16 +7,21 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         try {
-            JsonObject response = Pokemon.getRandomPokemon();
-            String pokemonName = response.get("name").getAsString();
-            String secretName = "";
-            for (int i = 0; i < pokemonName.length(); i += 1) {
-                secretName = secretName.concat("_ ");
-            }
-            System.out.println(secretName);
+            String[] response = Pokemon.getPokemonName();
+//            System.out.println(response[0]);
 
+            String welcomeMessage = """
+                    Hello world!
+                    This is a Pokémon-based hangman game!
+                    The application will sort a pokémon id from 1 to 151...
+                    And your mission is to guess the pokémon's name!
+                    """;
+
+            System.out.println(welcomeMessage);
+            Thread.sleep(500);
+            System.out.println(response[1]);
             Scanner scanner = new Scanner(System.in);
         } catch (Exception e) {
             e.printStackTrace();
