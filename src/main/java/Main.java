d6e9758e644cc65dcace774f49361f5ca92607d8
biancashiromoto <b.shiromoto.bs@gmail.com>
2023-10-27 20:24:08 -0300

    feat: Get pokemon name

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,23 @@
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import utils.Pokemon;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Pokemon pokemon = new Pokemon();
-        String response = pokemon.fetch();
-        System.out.println(response);
+        try {
+            JsonObject response = Pokemon.getRandomPokemon();
+            JsonElement pokemonName = response.get("name");
+
+
+            Scanner scanner = new Scanner(System.in);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
